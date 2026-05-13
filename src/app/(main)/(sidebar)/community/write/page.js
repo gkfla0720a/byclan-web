7e8fad80ab5250ad49fa43b44c5623c12887a8df
gkfla0720a <gkfla0720a@gmail.com>
2026-05-14 01:22:07 +0900

    page.js 업데이트

--- src/app/(main)/(sidebar)/community/write/page.js
+++ src/app/(main)/(sidebar)/community/write/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '@/supabase';
 import { useAuthContext } from '@/context/AuthContext';
